d733f8bbd5fba763f380640fc89e759614c1f2ec
Avior Kasay <aviork9@gmail.com>
2025-12-02 18:28:38 +0200

    FIX flicky test -testAddBackpackToCart

--- src/test/java/com/avior/selenium/pages/ProductsPage.java
+++ src/test/java/com/avior/selenium/pages/ProductsPage.java
@@ -5,6 +5,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 
 public class ProductsPage {
@@ -26,6 +30,9 @@ public class ProductsPage {
     public void navigateToProductPage(String productName) {
         WebElement productLink = driver.findElement(By.linkText(productName));
         productLink.click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.urlContains("inventory-item.html"));
+
     }
 
     public void navigateToCart() {
